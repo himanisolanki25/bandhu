5c23542f92a3ac692c879a674926845fa90c9320
himanisolanki25 <himanisolanki1000@gmail.com>
2025-01-19 23:50:24 +0530

    tailwind-file-upadated

--- backend/frontend/tailwind.config.js
+++ backend/frontend/tailwind.config.js
@@ -1,4 +1,26 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         lightPink: '#FAB3A9', // Example custom color
+//         darkPink: '#ED6B86', // Example custom color
+//         gray: '#BAA898', // Example custom color
+//         myBrown: '#463239', // Example custom color
+//         lightGray: '#EEEBD0', // Example custom color
+//         one: '#C4BBAE', // Example custom color
+//         two: '#A5978B', // Example custom color
+//         textBrown: '#280003', // Example custom color
+//       },
+//     },
+//   },
+//   plugins: [require('daisyui')],
+// }
+
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -15,6 +37,5 @@ export default {
       },
     },
   },
-  plugins: [require('daisyui')],
-}
-
+  plugins: [daisyui],
+};
